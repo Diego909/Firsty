b883e7588e8b7c28912ced23ec51138f255a4e04
Diego <diego909808@gmail.com>
2018-03-12 02:34:11 +0100

    Añadiendo dos ejercios de nate (adivina_un_numero_2 y calculadora). Mañana se añadira el ultimo ejercicio y posiblemente se intente mejorar el codigo de estos dos ultimos

--- adivina_un_numero_2.py
+++ adivina_un_numero_2.py
@@ -1,1 +1,23 @@
-#adivina un numero. Juego entre dos jugadores. Se pide un numero para adivinar a un jugador y el otro debe adivinarlo. intentos infinitos hasta que este lo adivine. Intentar ocultar el numero. 
+#adivina un numero. Jueg# o entre dos jugadores. Se pide un numero para adivinar a un jugador y el otro debe adivinarlo. intentos infinitos hasta que este lo adivine. Intentar ocultar el numero.
+
+print("Este es un juego para dos jugadores. Un jugador eligira un numero entre el 1 y el 10 y el otro jugador tiene 3 intentos para adivinarlo. Se especificara si es mayor o menor en caso de no acertar")
+
+jugador1 = int(input("Dime un numero para adivinar:"))
+oportunidades = 3
+
+print("Turno del segundo jugador")
+
+while oportunidades >= 1:
+    jugador2 = int(input("Adivina el numero: "))
+
+    if jugador1 < jugador2:
+        print("El numero es mas pequeño")
+        oportunidades -= 1
+    elif jugador1 > jugador2:
+        print("El numero es mas grande")
+        oportunidades -= 1
+    else:
+        print("Has adivinado el numero!")
+        oportunidades = 0
+
+print("El juego ha terminado")
--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,29 @@
 # Create a calculator that's ask to the user what kind of operation he want and two numbers for.
+
+repeticion = 1
+operacion = 0
+numero1 = 0
+numero2 = 0
+
+while repeticion > 0:
+    operacion = input("Que tipo de operacion deseas hacer: ( Suma / Resta / Multiplicacion / Division )").upper()
+
+    if operacion in ["SUMA", "RESTA", "MULTIPLICACION", "DIVISION"]:
+        repeticion -= 1
+    else:
+        print("No te he entendido")
+
+numero1 = int(input("Di un numero: "))
+numero2 = int(input("Di un segundo numero: "))
+
+if operacion == "SUMA":
+    print("El resultado es", numero1 + numero2)
+elif operacion == "RESTA":
+    print("El resultado es", numero1 - numero2)
+elif operacion == "DIVISION":
+    print("El resultado es", numero1 / numero2)
+    print("El resto de la division es", numero1 % numero2)
+else:
+    print("El resultado es", numero1 * numero2)
+
+print("Calculo terminado")
